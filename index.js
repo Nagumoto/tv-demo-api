23cf88f11d694e8f7ea04e12e33d7f79e54f2233
Neils Madsen <generalyourfather@gmail.com>
2019-01-14 22:10:55 -0700

    Prepared my routes to res and req as necessary

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 4000
 
-app.get('/shows', (req, res) => res.send('Did you just Get something?'))
+app.use(bodyParser.json())
 
-app.post('/shows', (req, res) => res.send('Hey! You just Post things whenever you feel like it don\'t you?'))
+app.route('/') 
+    .get((req, res) => {
+        console.log('Welcome! Are you ready to begin?')
+        res.send('Welcome! Are you ready to begin?')})
 
-app.put('/shows', (req, res) => res.send('Oh sure, now you\'re putting things?'))
-
-app.delete('/shows', (req,res) => res.send('Beleted... I mean Deleted.'))
+app.route('/shows')
+    .get((res) => {
+        console.log('Did you just Get something?')
+        res.send('Did you just Get something?')})
+    .post((req, res) => { 
+        console.log('Hey! You just Post things whenever you feel like it don\'t you?')
+        res.send({
+            body:req.body,
+            response:'Hey! You just Post things whenever you feel like it don\'t you?'})})
+    .put((req, res) => {
+        console.log('Oh sure, now you\'re putting things?')
+        res.send({
+            body:req.body,
+            response:'Oh sure, now you\'re putting things?'})})
+app.delete('/shows/:name', (req, res) => res.send('Beleted... I mean deleted.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
