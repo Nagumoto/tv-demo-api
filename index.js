60646e0b5c43015cfa7a5996a4fab2e3d9f19219
Neils Madsen <generalyourfather@gmail.com>
2019-01-23 21:08:58 -0700

    handled CORS issues

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const port = 4000
+const tvShows = []
 
 app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.route('/') 
     .get((res) => {
         console.log('Welcome! Are you ready to begin?')
         res.send('Welcome! Are you ready to begin?')})
-
-let tvShows = [{
-    "name": "The Guild",
-    "rating": "3",
-    "image": "http://pop-verse.com/wp-content/uploads/2013/02/theguild.jpg"
-  }]
-
+    
 app.route('/shows')
     .get((req, res) => {
-        console.log('Did you just Get something?')
+        console.log('Did you just Get something?', tvShows)
         res.json(tvShows)
         })
     .post((req, res) => {
-        console.log('Hey! You just Post things whenever you feel like it don\'t you?')
+        tvShows.push(req.body)
+        console.log('Hey! You just Post things whenever you feel like it don\'t you?', tvShows)
         res.send(res.json(tvShows))})
     .put((req, res) => {
         console.log('Oh sure, now you\'re putting things?')
-        res.send({
-            body:req.body,
-            response:'Oh sure, now you\'re putting things?'})})
+        res.send(req.body)})
 app.delete('/shows/:name', (req, res) => res.send('Beleted... I mean deleted.'))
 
 
