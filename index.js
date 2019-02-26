d45a104b5f39a3e284c07ef507f2201b81a48d61
Neils Madsen <generalyourfather@gmail.com>
2019-02-25 20:09:28 -0700

    Added Heroku address for front end to CORS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const tvShows = []
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' || 'https://glacial-lowlands-35572.herokuapp.com')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', true)
