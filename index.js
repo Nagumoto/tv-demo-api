bccb9dde0bd264fca30bb7d6e20028fcf978ca6a
Neils Madsen <generalyourfather@gmail.com>
2019-05-08 15:38:58 -0600

    added Mode as an allowed Header field

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Mode')
     res.setHeader('Access-Control-Allow-Credentials', true)
     next()
 })
